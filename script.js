99549c5adcbf4280729fc42633350c6d7d0064e5
jtearre <jeremy.hinman@gmail.com>
2024-11-11 10:35:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-
 function sendEmail() {
+  const toName = document.getElementById("to_name").value;
   const recipient = document.getElementById("recipient").value;
+  const fromName = document.getElementById("from_name").value;
   const message = document.getElementById("message").value;
 
   emailjs.send("service_r9gr1w7", "template_w0ef0uq", {
+    to_name: toName,
+    from_name: fromName,
     recipient_email: recipient,
-    message_content: message
+    message: message
   }, "q20xTMI3E1Pn9hm6s")
   .then(response => {
     alert("Email sent successfully!");
